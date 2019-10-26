9df79946e7a256c7a0324f9f9ab81d564f1e98d0
Zhiqiang Qian <zhiqiang.qian@adesso.de>
2019-10-26 22:49:18 +0200

    exercise 1.3

--- part1/courseinfo/src/index.js
+++ part1/courseinfo/src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 const Header = (props) => props ? (<h1>{props.course}</h1>) : null;
-const Part = (props) => props && props.content ?  (<p>{props.content.part} {props.content.exercises}</p>) : null;
+const Part = (props) => props && props.content ?  (<p>{props.content.name} {props.content.exercises}</p>) : null;
 const Content = (props) =>  (props &&  props.contents) ?
      props.contents.map((content, index) => (<Part key={index} content={content}/>)):
      null;
@@ -10,27 +10,24 @@ const Total = (props) => props ? (<p>Number of exercises {props.total}</p>) : nu
 
 const App = () => {
   const course = 'Half Stack application development';
-  const part1 = 'Fundamentals of React';
-  const exercises1 = 10;
-  const part2 = 'Using props to pass data';
-  const exercises2 = 7;
-  const part3 = 'State of a component';
-  const exercises3 = 14;
+  const part1 = {
+      name: 'Fundamentals of React',
+      exercises: 10
+    };
+  const part2 = {
+      name: 'Using props to pass data',
+      exercises: 7
+    };
+  const part3 = {
+      name: 'State of a component',
+      exercises: 14
+    };
   const contents =   [
-      {
-      part: part1,
-      exercises: exercises1
-     },
-     {
-        part: part2,
-        exercises: exercises2
-     },
-    {
-        part: part3,
-        exercises: exercises3
-    },
+      part1,
+      part2,
+      part3
 ];
-const total = exercises1 + exercises2 + exercises3;
+const total = part1.exercises + part2.exercises + part3.exercises;
 
   return (
     <div>
