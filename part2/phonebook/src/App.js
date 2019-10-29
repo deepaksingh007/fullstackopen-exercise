e8e64437e9c4c0e75a020dbfa092b41b3eaf2844
Zhiqiang Qian <zhiqiang.qian@adesso.de>
2019-10-29 11:38:01 +0100

    2.6

---
+++ part2/phonebook/src/App.js
@@ -0,0 +1,33 @@
+import React, { useState } from 'react';
+
+const App = () => {
+  const [ persons, setPersons] = useState([
+    { name: 'Arto Hellas' }
+  ]) ;
+  const [ newName, setNewName ] = useState('');
+  const addNumber = event => {
+    event.preventDefault();
+    setPersons(
+                persons.concat({name: newName})
+              );
+  };
+  const handleNewPerson = event => setNewName(event.target.value);
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form onSubmit={addNumber}>
+        <div>
+          name: <input onChange={handleNewPerson}/>
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      ...
+    </div>
+  );
+};
+
+export default App;
