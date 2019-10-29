36b6789b15ae3573387739a0cd210bd035303158
Zhiqiang Qian <zhiqiang.qian@adesso.de>
2019-10-29 09:57:47 +0100

    2.4

--- part2/course-contents/src/index.js
+++ part2/course-contents/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const Header = props => (<h1>{props.course}</h1>);
+const Header = props => (<h3>{props.course}</h3>);
 
 const Total = ({parts}) => {
     if(!parts) return null;
@@ -9,7 +9,7 @@ const Total = ({parts}) => {
     return (<p> <strong>total of {total} exercises</strong></p>);
 };
 
-const Part = props => (<p>{props.part.name} {props.part.exercises}</p>);
+const Part = ({part}) => (<p>{part.name} {part.exercises}</p>);
 
 const Content = ({ parts }) => parts ? (
     <div>
@@ -26,35 +26,55 @@ const Course = ({ course }) => course ? (
     null;
 
 const App = () => {
-    const course = {
-        name: 'Half Stack application development',
-        parts: [
-            {
-                name: 'Fundamentals of React',
-                exercises: 10,
+    const courses = [
+        {
+            name: 'Half Stack application development',
+            id: 1,
+            parts: [
+                {
+                    name: 'Fundamentals of React',
+                    exercises: 10,
+                    id: 1
+                },
+                {
+                    name: 'Using props to pass data',
+                    exercises: 7,
+                    id: 2
+                },
+                {
+                    name: 'State of a component',
+                    exercises: 14,
+                    id: 3
+                },
+                {
+                    name: 'Redux',
+                    exercises: 11,
+                    id: 4
+                },
+            ]
+        },
+        {
+            name: 'Node.js',
+            id: 2,
+            parts: [
+              {
+                name: 'Routing',
+                exercises: 3,
                 id: 1
-            },
-            {
-                name: 'Using props to pass data',
+              },
+              {
+                name: 'Middlewares',
                 exercises: 7,
                 id: 2
-            },
-            {
-                name: 'State of a component',
-                exercises: 14,
-                id: 3
-            },
-            {
-                name: 'Redux',
-                exercises: 11,
-                id: 4
-            },
-        ]
-    };
+              }
+            ]
+          }
+    ];
 
     return (
         <div>
-            <Course course={course} />
+            <h1>Web development curriculum</h1>
+           {courses.map( course => (<Course key={course.id} course={course} />)) }
         </div>
     );
 };
