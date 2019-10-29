bc161a82a035c33e9da65e996027e81e2c129193
Zhiqiang Qian <zhiqiang.qian@adesso.de>
2019-10-29 16:29:39 +0100

    2.11

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,15 +1,11 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import Persons from './Persons';
 import PersonForm from './PersonForm';
 import Filter from './Filter';
 
 const App = () => {
-  const [ persons, setPersons] = useState([
-        { name: 'Arto Hellas', number: '040-123456' },
-        { name: 'Ada Lovelace', number: '39-44-5323523' },
-        { name: 'Dan Abramov', number: '12-43-234345' },
-        { name: 'Mary Poppendieck', number: '39-23-6423122' }
-  ]) ;
+  const [ persons, setPersons] = useState([]) ;
   const [ newName, setNewName ] = useState('');
   const [ newNumber, setNewNumber ] = useState('');
   const [ filter, setFilter ] = useState('');
@@ -29,6 +25,14 @@ const App = () => {
       const value = event.target.value;
       setFilter(value);  
     };
+ const effect = () => {
+     axios.get('http://localhost:3001/persons')
+     .then(
+         ({data}) => setPersons(data),
+         error => console.error(error)
+     )    
+ };
+ useEffect(effect, []);
   return (
     <div>
       <h2>Phonebook</h2>
