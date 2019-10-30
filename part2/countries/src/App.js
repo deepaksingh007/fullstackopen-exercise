ac0716d8ffe58086c14ad2547b7321424c309007
Zhiqiang Qian <zhiqiang.qian@adesso.de>
2019-10-30 10:33:42 +0100

    2.14

--- part2/countries/src/App.js
+++ part2/countries/src/App.js
@@ -1,13 +1,16 @@
 import React, {useState, useEffect} from 'react';
 import axios from 'axios';
-import Countries from "./Countries";
+import {Countries, CountryDetails} from "./Countries";
 
+//it should work when a valid api key from weatherstack.com is used
+const API_KEY = 'some api key';
 
 function App() {
  const [countries, setCountries] = useState([]);
+ const [weather, setWeather] = useState(null);
  const [search, setSearch] = useState('');
  const handleSearch = event => setSearch(event.target.value);
- const effect = () => {
+ const searchEffect = () => {
     if(search) {
         axios.get(`https://restcountries.eu/rest/v2/name/${search}`)
             .then(
@@ -19,12 +22,31 @@ function App() {
                 )
             }
 };
-useEffect(effect,[search]);
+useEffect(searchEffect,[search]);
+const weatherEffect = () => {
+    if(countries && countries.length===1) {
+        axios.get('http://api.weatherstack.com/current', {params:{access_key: API_KEY, query:countries[0].capital}})
+            .then(
+                    ({data}) => {
+                        console.log('weather', data);
+                        setWeather(data);
+                    },
+                    error => {
+                        setWeather(null);
+                        console.error(error);
+                    }
+                )
+            }
+};
+useEffect(weatherEffect,[countries]);
 
   return (
     <div className="App">
       find countries <input onChange={handleSearch}/>
-      <Countries countries={countries} setCountries={setCountries}/>
+      { countries.length !== 1 ? 
+      <Countries countries={countries} setCountries={setCountries}/> :
+      <CountryDetails country={countries[0]} weather={weather}/>
+      }
     </div>
   );
 }
--- part2/countries/src/Countries.js
+++ part2/countries/src/Countries.js
@@ -1,23 +1,34 @@
 import React from 'react';
-const Country = ({country, showDetail, setCountries}) => {
+const CountryDetails= ({country, weather}) => {
+    if(!country || !weather) return null;
+    const {temperature, wind_dir, wind_speed, weather_icons} = weather.current;
+    return (
+        <div>
+          <h1>{country.name}</h1>
+          <p>capital {country.capital}</p>   
+          <p>population {country.population}</p>
+          <h3>languages</h3>
+          {country.languages.map(language => (<p key={language.name}>{language.name}</p>))}
+          <img src={country.flag} alt="flag" width="100" height="100"/>
+          <h3>weather in {weather.location.name}</h3>
+          <p><strong>temperature:</strong> {temperature}</p>
+          <img alt="weather_icon" src={weather_icons[0]} width="40" height="40"/>  
+          <p><strong>wind:</strong> {`${wind_speed}kph direction`} <strong>{`${wind_dir}`}</strong></p>
+        </div>
+      );
+}
+
+const Country = ({country, setCountries}) => {
+    if(!country) return null;
     const showCountry = () => setCountries([country]);
-    const details = (
-      <div>
-        <h1>{country.name}</h1>
-        <p>capital {country.capital}</p>   
-        <p>population {country.population}</p>
-        <h3>languages</h3>
-        {country.languages.map(language => (<p key={language.name}>{language.name}</p>))}
-        <img src={country.flag} alt="flag" width="100" height="100"/>
-      </div>
-    );
-    return showDetail ? details: (<p key={country.name}>{country.name}<button onClick={showCountry}>show</button></p>)
+    const countryName = (<p key={country.name}>{country.name}<button onClick={showCountry}>show</button></p>);
+    return countryName;
 };
+
 const Countries = ({countries, setCountries}) => {
     if(!countries) return null;
-    const showDetail =  countries.length === 1 ? true : false;
-    return countries.length <= 10 ? (<div>{countries.map(country => (<Country key={country.name} country={country} setCountries={setCountries} showDetail={showDetail}/>))}</div>) :
+    return countries.length <= 10 ? (<div>{countries.map(country => (<Country key={country.name} country={country} setCountries={setCountries}/>))}</div>) :
     (<p>Too many matches, specify another filter</p>);
 };
 
-export default Countries;
+export {Countries, CountryDetails};
