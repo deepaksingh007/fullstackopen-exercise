76893e0acfc02394dff0cc86bf88647a13f44191
Zhiqiang Qian <zhiqiang.qian@adesso.de>
2019-12-03 14:12:26 +0100

    GraphQl

---
+++ part8/phonebook-graphql/index.js
@@ -0,0 +1,161 @@
+const { ApolloServer, gql } = require('apollo-server')
+
+let authors = [
+  {
+    name: 'Robert Martin',
+    id: "afa51ab0-344d-11e9-a414-719c6709cf3e",
+    born: 1952,
+  },
+  {
+    name: 'Martin Fowler',
+    id: "afa5b6f0-344d-11e9-a414-719c6709cf3e",
+    born: 1963
+  },
+  {
+    name: 'Fyodor Dostoevsky',
+    id: "afa5b6f1-344d-11e9-a414-719c6709cf3e",
+    born: 1821
+  },
+  { 
+    name: 'Joshua Kerievsky', // birthyear not known
+    id: "afa5b6f2-344d-11e9-a414-719c6709cf3e",
+  },
+  { 
+    name: 'Sandi Metz', // birthyear not known
+    id: "afa5b6f3-344d-11e9-a414-719c6709cf3e",
+  },
+]
+
+/*
+ * It would be more sensible to assosiate book and the author by saving 
+ * the author id instead of the name to the book.
+ * For simplicity we however save the author name.
+*/
+
+let books = [
+  {
+    title: 'Clean Code',
+    published: 2008,
+    author: 'Robert Martin',
+    id: "afa5b6f4-344d-11e9-a414-719c6709cf3e",
+    genres: ['refactoring']
+  },
+  {
+    title: 'Agile software development',
+    published: 2002,
+    author: 'Robert Martin',
+    id: "afa5b6f5-344d-11e9-a414-719c6709cf3e",
+    genres: ['agile', 'patterns', 'design']
+  },
+  {
+    title: 'Refactoring, edition 2',
+    published: 2018,
+    author: 'Martin Fowler',
+    id: "afa5de00-344d-11e9-a414-719c6709cf3e",
+    genres: ['refactoring']
+  },
+  {
+    title: 'Refactoring to patterns',
+    published: 2008,
+    author: 'Joshua Kerievsky',
+    id: "afa5de01-344d-11e9-a414-719c6709cf3e",
+    genres: ['refactoring', 'patterns']
+  },  
+  {
+    title: 'Practical Object-Oriented Design, An Agile Primer Using Ruby',
+    published: 2012,
+    author: 'Sandi Metz',
+    id: "afa5de02-344d-11e9-a414-719c6709cf3e",
+    genres: ['refactoring', 'design']
+  },
+  {
+    title: 'Crime and punishment',
+    published: 1866,
+    author: 'Fyodor Dostoevsky',
+    id: "afa5de03-344d-11e9-a414-719c6709cf3e",
+    genres: ['classic', 'crime']
+  },
+  {
+    title: 'The Demon',
+    published: 1872,
+    author: 'Fyodor Dostoevsky',
+    id: "afa5de04-344d-11e9-a414-719c6709cf3e",
+    genres: ['classic', 'revolution']
+  },
+]
+
+const typeDefs = gql`
+type Author {
+  name: String!
+  id: ID!
+  born: Int
+  bookCount: Int
+}
+  type Book {
+    title: String!
+    published: Int!
+    author: String!
+    id: ID!
+    genres: [String!]!
+  }
+
+  type Query {
+    hello: String!
+    bookCount: Int!
+    authorCount: Int!
+    allBooks(author: String, genre: String): [Book!]!
+    allAuthors: [Author!]!
+  }
+
+  type Mutation {
+    addBook(title: String!
+    author: String!
+    published: Int!
+    genres: [String!]!): Book
+  }
+`
+
+const resolvers = {
+  Query: {
+    hello: () => { return "world" },
+    bookCount: () => books.length,
+    authorCount: () => authors.length,
+    allBooks: (root, args) => books.filter(book => (args.author === undefined || book.author === args.author) && (args.genre === undefined || book.genres.includes(args.genre))),
+    allAuthors: () => {
+      authors = authors.map(author => countBooks(author))
+    console.log(authors)
+    return authors
+  }
+  },
+  Mutation:{
+    addBook: (root, args) => {
+      const book = args
+      console.log(book)
+      books = books.concat({...book})
+      const authorNames = authors.map(author => author.name)
+      if(authorNames.includes(book.author)){
+        authors = authors.map(author => ({...author, bookCount: author.name === book.author ? countBooks(author) + 1 : countBooks(author)}))
+      }
+      else {
+        authors = authors.concat({name: book.author, born: null, bookCount: 1})
+      }
+      console.log(authors)
+      return  book
+    }
+  } 
+}
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+})
+
+const countBooks = (author) => {
+  const bookCount = books.reduce( (acc, cur) => {
+    return cur.author === author.name ? acc + 1 : acc
+  } ,0)
+  return {...author, bookCount}
+}
+server.listen().then(({ url }) => {
+  console.log(`Server ready at ${url}`)
+})
