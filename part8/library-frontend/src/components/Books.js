c5919ee9f37aea2fa7d0868ad62a3e5c152d76db
Zhiqiang Qian <zhiqiang.qian@adesso.de>
2019-12-04 14:14:44 +0100

    query allBooks

--- part8/library-frontend/src/components/Books.js
+++ part8/library-frontend/src/components/Books.js
@@ -1,37 +1,56 @@
 import React from 'react'
+import { Query } from 'react-apollo'
+import gql from 'graphql-tag'
 
 const Books = (props) => {
   if (!props.show) {
     return null
   }
-
-  const books = []
-
+  const ALL_BOOKS = gql(`{
+    allBooks{
+      title
+      author
+      published
+    }
+  }`)
   return (
-    <div>
-      <h2>books</h2>
+    <Query query={ALL_BOOKS}>
+      {(result) => {
+        if (result.loading) {
+          return <p>loading</p>
+        }
+        else {
+          const { allBooks: books } = result.data
+          return (
+            <div>
+              <h2>books</h2>
 
-      <table>
-        <tbody>
-          <tr>
-            <th></th>
-            <th>
-              author
-            </th>
-            <th>
-              published
-            </th>
-          </tr>
-          {books.map(a =>
-            <tr key={a.title}>
-              <td>{a.title}</td>
-              <td>{a.author}</td>
-              <td>{a.published}</td>
-            </tr>
-          )}
-        </tbody>
-      </table>
-    </div>
+              <table>
+                <tbody>
+                  <tr>
+                    <th></th>
+                    <th>
+                      author
+                  </th>
+                    <th>
+                      published
+                  </th>
+                  </tr>
+                  {books.map(a =>
+                    <tr key={a.title}>
+                      <td>{a.title}</td>
+                      <td>{a.author}</td>
+                      <td>{a.published}</td>
+                    </tr>
+                  )}
+                </tbody>
+              </table>
+            </div>
+
+          )
+        }
+      }}
+    </Query>
   )
 }
 
